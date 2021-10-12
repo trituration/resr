0e00a9b24cfd2985db202c0c6c5e602ff8b498b7
Anton Tsukanov <ubercomrade@gmail.com>
2021-10-12 12:11:41 +0700

    fix

--- example/example.sh
+++ example/example.sh
@@ -1,6 +1,8 @@
-restriction_sites_hider.py find_by_pattern ./NC_005816.fna AGCCAG test.txt
-restriction_sites_hider.py remove_by_pattern ./NC_005816.fna test.txt AGCCAG NC_005816_pattern.fa
+#!/bin/bash
+
+restriction_sites_hider.py find_by_pattern ./NC_005816.fna AGCCAG test_sites_hider_by_pattern.txt
+restriction_sites_hider.py remove_by_pattern ./NC_005816.fna test_sites_hider_by_pattern.txt AGCCAG NC_005816_pattern.fa
 
 
-restriction_sites_hider.py find_known_sites ./NC_005816.fna ScaI test.txt
-restriction_sites_hider.py remove_known_sites ./NC_005816.fna test.txt ScaI NC_005816_known_sites.fa
+restriction_sites_hider.py find_known_sites ./NC_005816.fna ScaI test_sites_hider_known_sites.txt
+restriction_sites_hider.py remove_known_sites ./NC_005816.fna test_sites_hider_known_sites.txt ScaI NC_005816_known_sites.fa
